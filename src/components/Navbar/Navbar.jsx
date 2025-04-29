b760d73c4aaf5f21d4188485708bc454dd378a31
iamakashtechie <akash.techie01@gmail.com>
2025-04-29 22:53:27 +0530

    Update  Navbar and Notice components for improved styling and responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,18 +21,16 @@ const Navbar = () => {
 
 
     return (
-        <header className={`w-full h-16 fixed top-0 z-50 transition-all duration-300 ${scrolled
-                ? "bg-white/5 backdrop-blur-md shadow-lg"
-                : "bg-transparent"}`}>
-            <nav className={`max-w-[1700px] h-full mx-auto flex items-center ${scrolled ? 'justify-center' : 'justify-between'} max-sm:justify-center transition-all duration-300 px-4`}>
-                <div className="flex items-center cursor-pointer">
-                    <img src="./assets/imgs/IIEST_Shibpur_Logo.png" alt="Logo" className={`h-10 pr-4 ${scrolled && 'hidden'} max-sm:hidden transition-all duration-300`} />
-                    <h1 className={`text-xl font-[700] text-black w-24 leading-4 ${scrolled && 'hidden'} max-sm:hidden transition-all duration-300`}>STUDENTS' SENATE</h1>
+        <header className={`w-full h-16 sticky top-0 z-50 bg-[#DEF0F9] transition-all duration-300`}>
+            <nav className={`max-w-[1700px] h-full mx-auto flex items-center ${scrolled ? "justify-between" : "justify-center"} transition-all duration-300 px-4`}>
+                <div className={`flex items-center cursor-pointer ${scrolled ? "block" : "hidden"} transition-all duration-300`}>
+                    <img src="./assets/imgs/IIEST_Shibpur_Logo.png" alt="Logo" className={`h-10 pr-4 transition-all duration-300`} />
+                    <h1 className={`text-xl font-[700] text-black w-24 leading-4 transition-all duration-300`}>STUDENTS' SENATE</h1>
                 </div>
 
-                <div className="flex gap-20 font-[700] text-black">
+                <div className={`flex ${scrolled ? "gap-5" : "gap-20"} font-[700] text-black`}>
                     <a href="#" className="hover:underline">Home</a>
-                    <a href="/council" className="hover:underline">Events</a>
+                    <a href="/council" className="hover:underline">Council</a>
                     <a href="/about" className="hover:underline">About Us</a>
                 </div>
             </nav>
--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -7,7 +7,7 @@ const Notice = ({ source, title, date, link, className }) => {
             <span onClick={link} className='text-sm min-[430px]:text-sm sm:text-base md:text-lg lg:text-xl hover:underline'>{title}</span>
             <div className='flex justify-between items-center'>
                 <span className='text-[0.6rem] min-[430px]:text-[0.65rem] sm:text-xs md:text-[0.8rem] md:text-sm'>{date}</span>
-                <span className='text-[0.6rem] min-[430px]:text-[0.65rem] sm:text-xs md:text-[0.8rem] flex items-center gap-1 sm:gap-1.5 cursor-pointer hover:underline'>
+                <span className='text-[0.6rem] min-[430px]:text-[0.65rem] sm:text-xs md:text-[0.8rem] flex items-center gap-1 sm:gap-1.5 cursor-pointer hover:underline mb-1.5'>
                     <span>Download</span>
                     <span>
                         <svg className='w-3.5 sm:w-4 md:w-5' width="12" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
